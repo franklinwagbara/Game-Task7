892652bb70127bf16afead71e417e380f2014088
Franklin Wagbara <Franklin.Wagbara@eps.it>
2025-02-18 19:18:02 +0100

    Initial commit

---
+++ Game-Task7/Program.cs
@@ -0,0 +1,83 @@
+﻿
+// Singleton Game Instance
+public sealed class Game
+{
+    private static readonly Game instance = new();
+
+    // Explicit static constructor to tell C# compiler
+    // not to mark type as beforefieldinit
+    static Game()
+    {
+    }
+
+    private Game()
+    {
+    }
+
+    public static Game Instance => instance;
+
+    public World? World { get; set; }
+}
+
+// Cell Class
+public record Cell
+{
+    public string Name { get; init; } = "Empty";
+    public int X { get; init; }
+    public int Y { get; init; }
+}
+
+// World Class
+public class World
+{
+    public Cell[,] Grid { get; init; }
+    public int Width { get; init; }
+    public int Height { get; init; }
+
+    public World(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Grid = new Cell[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Grid[x, y] = new Cell { X = x, Y = y };
+            }
+        }
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        if (x < 0 || x >= Width || y < 0 || y >= Height)
+        {
+            throw new ArgumentOutOfRangeException("Coordinates are out of bounds.");
+        }
+        return Grid[x, y];
+    }
+
+    public void SetCellName(int x, int y, string name)
+    {
+        if (x < 0 || x >= Width || y < 0 || y >= Height)
+        {
+            throw new ArgumentOutOfRangeException("Coordinates are out of bounds.");
+        }
+        Grid[x, y] = Grid[x, y] with { Name = name };
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var game = Game.Instance;
+        var newWorld = new World(10, 8);
+        game.World = newWorld;
+
+        int x = 1;
+        int y = 1;
+        Console.WriteLine("Cell X = {0}, Y = {1}: {2}", x, y, game.World.GetCell(x, y));
+    }
+}
---
+++ GameTest/Test1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class GameTests
+{
+    [TestMethod]
+    public void Game_SingletonInstance_NotNull()
+    {
+        Assert.IsNotNull(Game.Instance);
+    }
+
+    [TestMethod]
+    public void World_Initialization_CreatesGridWithCorrectDimensions()
+    {
+        int width = 10;
+        int height = 5;
+        World world = new(width, height);
+
+        Assert.AreEqual(width, world.Width);
+        Assert.AreEqual(height, world.Height);
+        Assert.AreEqual(width, world.Grid.GetLength(0));
+        Assert.AreEqual(height, world.Grid.GetLength(1));
+    }
+
+    [TestMethod]
+    public void World_Initialization_CellsHaveCorrectCoordinates()
+    {
+        int width = 3;
+        int height = 2;
+        World world = new(width, height);
+
+        Assert.AreEqual(0, world.Grid[0, 0].X);
+        Assert.AreEqual(0, world.Grid[0, 0].Y);
+
+        Assert.AreEqual(1, world.Grid[1, 0].X);
+        Assert.AreEqual(0, world.Grid[1, 0].Y);
+
+        Assert.AreEqual(2, world.Grid[2, 0].X);
+        Assert.AreEqual(0, world.Grid[2, 0].Y);
+
+        Assert.AreEqual(0, world.Grid[0, 1].X);
+        Assert.AreEqual(1, world.Grid[0, 1].Y);
+
+        Assert.AreEqual(1, world.Grid[1, 1].X);
+        Assert.AreEqual(1, world.Grid[1, 1].Y);
+
+        Assert.AreEqual(2, world.Grid[2, 1].X);
+        Assert.AreEqual(1, world.Grid[2, 1].Y);
+    }
+
+    [TestMethod]
+    public void World_GetCell_ReturnsCorrectCell()
+    {
+        int width = 5;
+        int height = 5;
+        World world = new(width, height);
+
+        Cell cell = world.GetCell(2, 3);
+
+        Assert.AreEqual(2, cell.X);
+        Assert.AreEqual(3, cell.Y);
+    }
+
+    [TestMethod]
+    public void World_GetCell_OutOfBounds_ThrowsException()
+    {
+        int width = 5;
+        int height = 5;
+        World world = new(width, height);
+
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.GetCell(-1, 0));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.GetCell(0, -1));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.GetCell(width, 0));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.GetCell(0, height));
+    }
+
+    [TestMethod]
+    public void World_SetCellName_ChangesCellName()
+    {
+        int width = 2;
+        int height = 2;
+        World world = new(width, height);
+
+        string newName = "Mountain";
+        world.SetCellName(1, 0, newName);
+
+        Assert.AreEqual(newName, world.GetCell(1, 0).Name);
+    }
+
+    [TestMethod]
+    public void World_SetCellName_OutOfBounds_ThrowsException()
+    {
+        int width = 2;
+        int height = 2;
+        World world = new(width, height);
+
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.SetCellName(-1, 0, "Test"));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.SetCellName(0, -1, "Test"));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.SetCellName(width, 0, "Test"));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => world.SetCellName(0, height, "Test"));
+    }
+}
